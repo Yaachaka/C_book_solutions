84d67c0b7ec36c489ff5898d217618ef56e297a0
Yaachaka <72916025+Yaachaka@users.noreply.github.com>
2020-12-02 10:36:00 +0530

    Add files via upload

---
+++ Chap02: C Fundamentals/knkcch02e01.c
@@ -0,0 +1,20 @@
+/*
+--PROGRAM NAME: knkcch02e01.c
+--PROGRAM STATEMENT: knkcch02e01: Create and run Kerningham and Ritchie's 
+famous "hello, world" program:
+	#include <stdio.h>
+	
+	int main(void)
+	{
+		printf("hello, world\n");
+	}
+Do you get a warning message from the compiler? If so, what's needed to make it 
+go away?
+*/
+#include <stdio.h>
+	
+int main(void)
+{
+	printf("hello, world\n");
+	return 0;
+}
