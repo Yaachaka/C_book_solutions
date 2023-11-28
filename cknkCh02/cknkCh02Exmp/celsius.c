009199ab347599ceeaf9f91e23a99a84143ffb75
Yaachaka <rosarjuna@gmail.com>
2023-11-28 17:27:23 +0530

    Section 2.7 Identifiers::celsius.c file

---
+++ cknkCh02/cknkCh02Exmp/celsius.c
@@ -0,0 +1,24 @@
+/**
+ * File: celsius.c
+ * Author: K. N. King
+ * Purpose: Convertts a Fahrenheit temperature to Celsius
+ */
+
+#include <stdio.h>
+
+#define FREEZING_PT 32.0f
+#define SCALE_FACTOR (5.0f /9.0f)
+
+int main(void)
+{
+    float fahrenheit, celsius;
+
+    printf("Enter Fahrenheit temperature: ");
+    scanf("%f", &fahrenheit);
+
+    celsius = (fahrenheit - FREEZING_PT) * SCALE_FACTOR;
+
+    printf("Celsius equivalent: %.1f\n", celsius);
+
+    return 0;
+}
