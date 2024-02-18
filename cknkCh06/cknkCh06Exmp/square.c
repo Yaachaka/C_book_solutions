9690fefdefc05a0c8a4e8af25846f43cb4008268
Yaachaka <paramaarjuna@gmail.com>
2024-02-18 15:15:04 +0530

    ACT: square.c from Section 6.1 completed

---
+++ cknkCh06/cknkCh06Exmp/square.c
@@ -0,0 +1,25 @@
+/*
+ * file: square.c
+ * Purpose: Prints a table of squares using a while statement
+ * Author: K. N. King
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i, n;
+
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+
+    i = 1;
+    while(i <= n)
+    {
+        printf("%10d%10d\n", i, i * i);
+        i++;
+    }
+
+    return 0;
+}
