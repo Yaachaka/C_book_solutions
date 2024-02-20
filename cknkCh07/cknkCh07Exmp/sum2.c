aea57478500d634216b6a5b627edacb910afd7d4
Yaachaka <paramaarjuna@gmail.com>
2024-02-20 16:29:56 +0530

    ACT: sum2.c from Section 7.1 completed

---
+++ cknkCh07/cknkCh07Exmp/sum2.c
@@ -0,0 +1,25 @@
+/*
+ * file: sum2.c
+ * Purpose: Sums a series of numbers (using long variables)
+ * Author: K. N. King
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    long n, sum = 0;
+
+    printf("This program sums a series of integers.\n");
+    printf("Enter integers (0 to terminate): ");
+
+    scanf("%ld", &n);
+    while(n != 0)
+    {
+        sum += n;
+        scanf("%ld", &n);
+    }
+    printf("The sum is: %ld\n", sum);
+
+    return 0;
+}
