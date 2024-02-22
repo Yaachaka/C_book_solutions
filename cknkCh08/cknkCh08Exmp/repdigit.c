268c4726c7bbe299db67d5c2930079f11843c0b0
Yaachaka <paramaarjuna@gmail.com>
2024-02-22 16:18:26 +0530

    ACT: repdigit.c example from Section 8.1
    Section 8.1 One-dimensional arrays

---
+++ cknkCh08/cknkCh08Exmp/repdigit.c
@@ -0,0 +1,33 @@
+/**
+ * file:repdigit.c
+ * Author: K. N. King
+ * Purpose: Checks numbers for repeated digits */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    bool digit_seen[10] = {false};
+    int digit;
+    long n;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+
+    while(n > 0)
+    {
+        digit = n % 10;
+        if(digit_seen[digit])
+            break;
+        digit_seen[digit] = true;
+        n /= 10;
+    }
+
+    if(n > 0)
+        printf("Repeated digit\n");
+    else
+        printf("No repeated digit\n");
+
+    return 0;
+}
