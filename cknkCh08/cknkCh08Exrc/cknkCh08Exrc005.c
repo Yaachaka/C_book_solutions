7dd849a7d79b47549c5eb703143d78a054eb8c76
Yaachaka <paramaarjuna@gmail.com>
2024-02-23 11:33:32 +0530

    ACT: cknkCh08Exrc005 completed

--- cknkCh08/cknkCh08Exrc/cknkCh08Exrc005.c
+++ cknkCh08/cknkCh08Exrc/cknkCh08Exrc005.c
@@ -5,9 +5,20 @@
 
 #include <stdio.h>
 
+#define N 40
+
 int main(void)
 {
+    int fib_numbers[N] = {0, 1};
 
+    printf("First 40 Fibonacci numbers: %d %d", fib_numbers[0], fib_numbers[1]);
+
+    for(int i = 2; i < N; i++)
+    {
+        fib_numbers[i] = fib_numbers[i - 1] + fib_numbers[i - 2];
+        printf(" %d", fib_numbers[i]);
+    }
+    printf("\n");
 
     return 0;
 }
