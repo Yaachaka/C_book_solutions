1d8ae952c0217443169261a3a608051337648cfd
Yaachaka <paramaarjuna@gmail.com>
2024-02-27 10:11:36 +0530

    ACT: "Chapter 09: functions" examples completed
    average.c
    countdown.c
    prime.c
    pun2.c
    qsort.c

---
+++ cknkCh09/cknkCh09Exmp/prime.c
@@ -0,0 +1,35 @@
+/**
+ * file: prime.c
+ * Author: K. N. King
+ * Purpose: Tests whether a number is prime
+ */
+
+#include <stdbool.h> /* C99 only */
+#include <stdio.h>
+
+bool is_prime(int n)
+{
+    int divisor;
+
+    if(n <= 1)
+        return false;
+    for(divisor = 2; divisor * divisor <= n; divisor++)
+        if(n % divisor == 0)
+            return false;
+    return true;
+}
+
+int main(void)
+
+{
+    int n;
+
+    printf("Enter a nuwber: ");
+    scanf ("%d", &n);
+    if(is_prime(n))
+        printf("Prime\n") ;
+    else
+        printf("Not prime\n");
+
+    return 0;
+}
